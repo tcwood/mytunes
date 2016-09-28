7adf01ab9492cbed044bbae230deb90658f52ee3
tcwood <travis.cwood@gmail.com>
2016-09-28 16:29:00 -0700

    completed song model and song queue specs

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,35 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function(song) {
+      if (this.length === 1) {
+        this.playFirst();
+      }  
+    }, this);
+
+    this.on('ended', function(song) {
+      this.shift();
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function(song) {
+      this.remove(song);
+    });
+  },
+
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
+
+// when a song is added
+  //check if it is only song in queue
+    //if it is, play song (playFirst?)
+    //else, nothing
+
+
+
+
